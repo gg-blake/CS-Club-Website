d053f152b2c506193201847bd28a1df51363e89a
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2023-04-09 22:15:30 -0400

    ui updates to officer profiles

--- src/app/components/profile-card.tsx
+++ src/app/components/profile-card.tsx
@@ -6,18 +6,20 @@ export default function ProfileCard({ children , name="Generic Profile" , title=
     const [expanded, setExpanded] = useState(false);
     
     return (
-        <div className={`relative w-[300px] transition-all ${expanded ? "h-auto" : "h-[90vh] overflow-y-clip"}`}>
-            <div className="w-full h-auto p-5">
-                <img src={src.src} alt={src.src} className="w-full h-auto rounded-full shadow-xl" />
+        <div className={`relative w-[300px] flex flex-col transition-all h-[70vh] gap-2 border-[2px] border-secondary-800 p-3 rounded-md`}>
+            <div className="w-full h-auto">
+                <img src={src.src} alt={src.src} className="w-full h-auto rounded-md shadow-xl" />
             </div>
-            <h1 className="text-3xl text-primary-400 font-medium leading-tight text-center">{name}</h1>
-            <h2 className="text-lg text-secondary-300 font-thin leading-tight mb-2 text-center">{title}</h2>
-            <div className="w-full h-auto flex justify-center gap-2">
-                {links?.map((link, index) => <a className="px-1 py-[1px] border-[1px] border-primary-300 text-primary-300 text-xs underline hover:no-underline underline-offset-[2px]" href={link.href} key={index}>{link.name}</a>)}
+            <span className="leading-tight text-secondary-200">
+                <h1 className="text-3xl font-semibold">{name}</h1>
+                <h2 className="text-lg font-thin">{title}</h2>
+            </span>
+            <div className="w-[92%] h-[3px] my-[5px] bg-secondary-600 rounded-full self-center" />
+            <div className="w-full h-auto flex gap-3">
+                {links?.map((link, index) => <a className="px-1 py-[1px] rounded-sm border-[1px] border-primary-500 text-primary-500 text-xs hover:bg-primary-500 hover:border-secondary-200 hover:text-secondary-200" href={link.href} key={index}>{link.name}</a>)}
             </div>
-            <p className={`w-full h-auto transition-all z-10 flex flex-col`}>
+            <p className={`w-full transition-all z-10 flex flex-col flex-shrink h-full overflow-y-scroll`}>
                 {children}
-                <div className={`${!expanded ? "absolute bottom-0" : ""} z-20 w-full h-[30px] flex items-end bg-secondary-900 text-secondary-300`} onClick={() => setExpanded(!expanded)}>{expanded ? "show less" : "show more"}</div>
             </p>
             
         </div>
