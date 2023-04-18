aa800ca8830462a2f18c631fe980116a7c4d83c4
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2023-04-18 10:02:21 -0400

    edit styles for generic button

--- src/app/components/generic-button.tsx
+++ src/app/components/generic-button.tsx
@@ -1,16 +1,13 @@
-import React, { ForwardedRef } from 'react';
+import React, { ForwardedRef, ReactNode } from 'react';
 import useForwardRef from "@/hooks/useForwardRef";
 
-const GenericButton = React.forwardRef(({ children , className }: { children? : string | JSX.Element , className? : string }, ref: ForwardedRef<HTMLDivElement>) => {
-    const thisElementRef = useForwardRef<HTMLDivElement>(ref);
+const GenericButton = React.forwardRef(({ children , className , ...buttonProps }: { children? : ReactNode , className? : string , buttonProps? : any }, ref: ForwardedRef<HTMLButtonElement>) => {
+    const thisElementRef = useForwardRef<HTMLButtonElement>(ref);
     
     return (
-        <>
-        <div ref={thisElementRef} className={`peer bg-secondary-800 hover:bg-secondary-700 hover:scale-[105%] transition-all z-30 ${ className }`}>
+        <button ref={thisElementRef} {...buttonProps} className="w-[130px] h-auto px-3 py-2 text-center border-[1px] mt-3 rounded-sm border-primary-500 hover:border-secondary-200 text-sm text-primary-500 hover:text-secondary-200 hover:bg-primary-500 transition-colors ">
             { children }
-        </div>
-        
-        </>
+        </button>
     )
 });
 
