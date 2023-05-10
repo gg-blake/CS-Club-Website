12d7fcf4e21d61336c7e6e350f7694a6018f9c3a
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2023-05-09 23:47:10 -0400

    title description opacity decreased & translation

--- src/app/components/sections/splash.tsx
+++ src/app/components/sections/splash.tsx
@@ -1,10 +1,10 @@
 import { useEffect , useState , useContext } from 'react';
 import { LangContext } from '@/app/components/context/lang-context';
-import MatrixCanvas from '@/app/components/core/matrix-canvas';
+import MatrixCanvas from '@/app/components/core/MatrixCanvas';
 import LangContent from '@/app/components/types/lang-content';
-import GlitchText from '@/app/components/core/glitch-text';
+import GlitchText from '@/app/components/core/GlitchText';
 import "@/app/globals.css";
-import ThoughtBubble from '../core/thought-bubble';
+import ThoughtBubble from '../core/ThoughtBubble';
 
 export default function Splash({ title, desc } : { title : LangContent , desc : LangContent}) {
     const { lang } = useContext(LangContext);
@@ -41,6 +41,11 @@ export default function Splash({ title, desc } : { title : LangContent , desc : 
         <div className='z-10 relative h-screen overflow-clip p-0 m-0'>
             <div id="splash" className={`absolute z-10 top-0 w-screen h-screen flex flex-col justify-center`}>
                 <MatrixCanvas scroll={scroll} />
+                {lang === 'jp' && <div className="w-3/5 h-auto absolute bottom-0 left-0 z-10">
+                        <img className="opacity-10 scale-x-[-100%]" src="/splash-emoji.png" />
+                        <p className="absolute bottom-[39%] left-[29%] skew-x-[30deg] rotate-[-28deg] font-bold text-7xl text-primary-500 animate-pulse">友</p>
+                        <p className="absolute bottom-[23%] left-[9%] skew-x-[30deg] rotate-[-28deg] font-bold text-7xl text-primary-500 animate-pulse">会</p>
+                    </div>}
                 <div id="title" className='my-2 px-3 z-10 relative'>
                     <p className='text-3xl text-primary-200 px-2 py-2 font-extrabold'>{{
                         'en': 'UMass Boston',
@@ -52,15 +57,18 @@ export default function Splash({ title, desc } : { title : LangContent , desc : 
                         <ThoughtBubble>
                             {{
                                 'en': 'Find us at UHall, Room 2320',
-                                'jp': 'ゆにばーしてい・ほーるの2320部屋にいます',
+                                'jp': 'ゆにばーしてい・ほーるの2320へやにいます',
                             }[lang] as string}
                         </ThoughtBubble>
                     </span>
                     
-                    <p className='text-xl text-secondary-100 px-2 py-2'>{desc[lang]}</p>
+                    <p className='text-xl text-secondary-100 px-2 py-2 opacity-70'>{desc[lang]}</p>
                     
                 </div>
+                
+                
             </div>
+            
             <div className="absolute bottom-[20px] z-10 w-full h-auto flex flex-col items-center ">
                 {touching && <><span className="text-base text-secondary-100 animate-pulse font-medium animate-bounce">{{
                     "en": "Scroll to learn more",
