dea143579f954c349dd4a530efce26d2b511a1fc
Blake Moody <32070003+TheSwiginator@users.noreply.github.com>
2023-07-03 22:51:17 -0400

    jetbrains mono font added omgomgomg

--- src/app/styles/fonts.ts
+++ src/app/styles/fonts.ts
@@ -1,6 +1,7 @@
 import { Inter, M_PLUS_1p } from "next/font/google";
+import localFont from 'next/font/local';
 
-const LATIN = Inter({ subsets: ['latin'] });
+const LATIN = localFont({src:'JetBrainsMono-Regular.woff2'});
 const KANA = M_PLUS_1p({ subsets: ['latin'], weight: ["400", "500", "700", "800", "900"]});
 
 export { LATIN, KANA };
